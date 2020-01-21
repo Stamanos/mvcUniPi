10db3ac7187d1d3b05b664a22bb18ed38da7cc05
Manos Stamatakis <themagicsport@gmail.com>
2020-01-21 20:05:46 +0200

    gamiesai

---
+++ University/Models/EnrollmentMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace University.Models
+{
+    public class EnrollmentMetadata
+    {
+        [Range(0, 4)]
+        public Nullable<decimal> Grade;
+    }
+}
---
+++ University/Models/PartialClasses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace University.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+
+    [MetadataType(typeof(EnrollmentMetadata))]
+    public partial class Enrollment
+    {
+    }
+}
